206b5c0bf44d28aa605a6907b946bab62f49bb49
Artem Bilodid <bilodid.sumy@gmail.com>
2021-11-20 21:04:03 +0200

    reimplemented App.js to use hooks

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,51 +1,59 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import styles from './app.module.scss';
 import Section from '../section';
 import Statistics from '../statistics';
 import Notification from '../notification';
 import FeedbackOptions from '../feedback-options';
 
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+const FEEDBACK_TYPE = {
+  GOOD: 'good',
+  NEUTRAL: 'neutral',
+  BAD: 'bad',
+};
 
-  handleFeedback = event => {
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleFeedback = event => {
     const { name } = event.target;
 
-    this.setState(prevState => ({
-      [name]: ++prevState[name],
-    }));
+    switch (name) {
+      case FEEDBACK_TYPE.GOOD:
+        return setGood(prev => prev + 1);
+      case FEEDBACK_TYPE.NEUTRAL:
+        return setNeutral(prev => prev + 1);
+      case FEEDBACK_TYPE.BAD:
+        return setBad(prev => prev + 1);
+      default:
+        throw new Error(`Unsupported feedback type: ${name}`);
+    }
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = good + neutral + bad;
-    const positivePercentage = Math.round((100 * good) / total, 0);
+  const total = good + neutral + bad;
+  const positivePercentage = Math.round((100 * good) / total, 0);
 
-    return (
-      <div className={styles.app}>
-        <Section title="Please leave feedback">
-          <FeedbackOptions options={{ good, neutral, bad }} onLeaveFeedback={this.handleFeedback} />
-        </Section>
-        <Section title="Statistics">
-          {total > 0 ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              positivePercentage={positivePercentage}
-            />
-          ) : (
-            <Notification message="No feedback given" />
-          )}
-        </Section>
-      </div>
-    );
-  }
-}
+  return (
+    <div className={styles.app}>
+      <Section title="Please leave feedback">
+        <FeedbackOptions options={{ good, neutral, bad }} onLeaveFeedback={handleFeedback} />
+      </Section>
+      <Section title="Statistics">
+        {total > 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positivePercentage}
+          />
+        ) : (
+          <Notification message="No feedback given" />
+        )}
+      </Section>
+    </div>
+  );
+};
 
 export default App;
